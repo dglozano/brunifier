e1d63de938391110497aa7ddcf912f936420be53
emigioria <emigioria@hotmail.com>
2017-09-16 12:25:26 -0300

    Quitado comentario

--- src/cp/pdp/st/FormatoEstandar.java
+++ src/cp/pdp/st/FormatoEstandar.java
@@ -51,10 +51,4 @@ public class FormatoEstandar extends ComponenteDeProcesamiento {
 		mapa.put(iApertura, GNUSmalltalk.Marca.aperturaBloque);
 		mapa.put(iCierre, GNUSmalltalk.Marca.cierreBloque);
 	}
-
-	/*
-	 * TODO Este modulo debe dejar todas las lineas sin indentaci�n y sin blancos al final de las lineas
-	 * (para poder preguntar por startWith y endsWith), sin lineas en blanco (para que todas las lineas sean procesables)
-	 * y con un �nico blanco antes de la instrucci�n y la llave que abre un bloque (o sea, por ejemplo "if() {").
-	 */
 }
