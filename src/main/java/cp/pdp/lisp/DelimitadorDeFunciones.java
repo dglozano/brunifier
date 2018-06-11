ae653faddf2933a26826c5b415d4f71304f19901
emigioria <emigioria@hotmail.com>
2018-06-11 11:45:35 -0300

    Corregidos bugs de Scheme

--- src/main/java/cp/pdp/lisp/DelimitadorDeFunciones.java
+++ src/main/java/cp/pdp/lisp/DelimitadorDeFunciones.java
@@ -14,15 +14,18 @@ public class DelimitadorDeFunciones extends ComponenteDeProcesamiento {
 		ArchivoScheme archivoTransformado = (ArchivoScheme) archivo;
 		//ArrayList es más eficiente para quitar al final, es decir, como pila
 		List<String> pilaDeFuncionesAbiertas = new ArrayList<>();
+		Integer[] idIfActual = new Integer[] { 0 };
 
+		String lambdaStr = Scheme.Marca.lambda.toString();
 		String aperturaStr = Scheme.Marca.aperturaBloque.toString();
 		String cierreStr = Scheme.Marca.cierreBloque.toString();
 		String ifStr = Scheme.Marca.iF.toString();
 		List<String> pilaIFOriginal = Arrays.asList(
-				"CAMINO POR FALSO DE " + ifStr,
-				"CAMINO POR VERDADERO DE " + ifStr,
+				"CAMINO FALSO DE " + ifStr,
+				"CAMINO VERDADERO DE " + ifStr,
 				"CONDICION DE " + ifStr);
 		List<String> pilaIF = new ArrayList<>();
+		List<Integer> pilaIdIF = new ArrayList<>();
 
 		archivoTransformado.getLineas().forEach(lineaOriginal -> {
 			Integer indice = 0;
@@ -34,10 +37,14 @@ public class DelimitadorDeFunciones extends ComponenteDeProcesamiento {
 					String nombreFuncion = lineaOriginal.getNombreFuncion(indice);
 					if(nombreFuncion != null){
 						if(nombreFuncion.contains(aperturaStr)){
-							nombreFuncion = "";
+							nombreFuncion = nombreFuncion.substring(0, nombreFuncion.indexOf(aperturaStr));
 						}
 						else if(nombreFuncion.contains(ifStr)){
+							idIfActual[0] = idIfActual[0] + 1;
 							pilaIF.addAll(pilaIFOriginal);
+							for(int i = 0; i < pilaIFOriginal.size(); i++){
+								pilaIdIF.add(idIfActual[0]);
+							}
 						}
 						else if(nombreFuncion.contains(cierreStr)){
 							nombreFuncion = nombreFuncion.substring(0, nombreFuncion.indexOf(cierreStr));
@@ -52,20 +59,35 @@ public class DelimitadorDeFunciones extends ComponenteDeProcesamiento {
 						indice = indiceCierre + cierreStr.length();
 
 						String funcionTopePila = pilaDeFuncionesAbiertas.remove(pilaDeFuncionesAbiertas.size() - 1);
+
+						if(funcionTopePila.equals(ifStr)){
+							while(!pilaIdIF.isEmpty() && idIfActual[0].equals(pilaIdIF.get(pilaIdIF.size() - 1))){
+								pilaIdIF.remove(pilaIdIF.size() - 1);
+								pilaIF.remove(pilaIF.size() - 1);
+							}
+							idIfActual[0] = idIfActual[0] - 1;
+						}
+
 						if(funcionTopePila.isEmpty()){
-							funcionTopePila = "Funcion de orden superior";
+							funcionTopePila = "FUNCION DE ORDEN SUPERIOR";
 						}
 						if(!pilaDeFuncionesAbiertas.isEmpty()){
 							String funcionContenedora = pilaDeFuncionesAbiertas.get(pilaDeFuncionesAbiertas.size() - 1);
 							if(funcionContenedora.isEmpty()){
-								funcionContenedora = "Funcion de orden superior";
+								funcionContenedora = "FUNCION DE ORDEN SUPERIOR";
 							}
 							if(funcionContenedora.equals(ifStr)){
 								String finalStr = pilaIF.remove(pilaIF.size() - 1);
+								pilaIdIF.remove(pilaIdIF.size() - 1);
 								lineaOriginal.setMarca("CIERRA " + funcionTopePila + " EN " + finalStr);
 							}
 							else{
-								lineaOriginal.setMarca("CIERRA " + funcionTopePila + " EN " + funcionContenedora);
+								if(!lineaOriginal.getCodigoLinea().contains(lambdaStr)){
+									lineaOriginal.setMarca("CIERRA " + funcionTopePila + " EN " + funcionContenedora);
+								}
+								else{
+									lineaOriginal.setMarca("CIERRA PARAMETROS EN " + funcionContenedora);
+								}
 							}
 						}
 						else{
@@ -77,6 +99,7 @@ public class DelimitadorDeFunciones extends ComponenteDeProcesamiento {
 							String funcionTopePila = pilaDeFuncionesAbiertas.get(pilaDeFuncionesAbiertas.size() - 1);
 							if(funcionTopePila.equals(ifStr)){
 								String finalStr = pilaIF.remove(pilaIF.size() - 1);
+								pilaIdIF.remove(pilaIdIF.size() - 1);
 								lineaOriginal.setMarca(finalStr);
 							}
 						}
